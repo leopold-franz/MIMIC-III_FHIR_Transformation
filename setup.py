d6a722439051ae252de856c5a93f881eec65f493
Leopold Franz <leopold.franz@gmx.ch>
2020-07-29 09:22:53 +0200

    Adding setup.py to install as a python package.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='mimic_fhir_transfrom',
+      version='0.0.1',
+      description='Python package to transform MIMIC-III tables to FHIR format',
+      url='https://github.com/leopold-franz/MIMIC-III_FHIR_Transformation',
+      packages=['mimic_fhir_transform'],
+      python_requires='>=3.7.7',
+      install_requires=[
+          "numpy==1.18.5",
+          "pandas==1.0.4",
+      ],
+      extras_requires={
+          'dev': ['jupyter'],
+      },
+      entry_points={
+      },
+      zip_safe=False)
